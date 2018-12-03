6b878e7ff93cf024cd3c51754893ddf6710aeece
Tuhin <tuhin@codesolz.net>
2018-12-03 08:50:37 +0600

    initial commit

--- read-emails-from-server.php
+++ read-emails-from-server.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Class : Email Reader
+ *
+ * @version 1.0
+ * @category Server Email
+ * @author Tuhin <tuhin@codesolz.net>
+ * @license http://URL MIT
+ */
+
 class Email_reader {
 
     // imap server connection
@@ -14,13 +23,20 @@ class Email_reader {
     private $pass = ''; //enter your email account password
     private $port = 110; // adjust according to server settings
 
-    // connect to the server and get the inbox emails
+    /**
+     * Init class
+     * connect to the server and get the inbox emails
+     * 
+     */
     function __construct() {
         $this->connect();
         $this->inbox();
     }
 
-    // close the server connection
+    /**
+     * close the server connection
+     * 
+     */
     function close() {
         $this->inbox = array();
         $this->msg_cnt = 0;
@@ -28,17 +44,24 @@ class Email_reader {
         imap_close($this->conn);
     }
 
-    // open the server connection
-    // the imap_open function parameters will need to be changed for the particular server
-    // these are laid out to connect to a Dreamhost IMAP server
+    /**
+     * open the server connection
+     * the imap_open function parameters will need to be changed for the particular server
+     * these are laid out to connect to a Dreamhost IMAP server
+     * 
+     */
     function connect() {
         $this->conn = imap_open('{' . $this->server . '/notls}', $this->user, $this->pass);
         //$this->conn =imap_open('{'.$this->server.':'.$this->port.'/imap/ssl/novalidate-cert}INBOX', $this->user, $this->pass) or die('Cannot connect: ' . print_r(imap_errors(), true));
     }
 
-    // move the message to a new folder
+    /**
+     * move the message to a new folder
+     * 
+     * @param type $msg_index
+     * @param type $folder
+     */
     function move($msg_index, $folder = 'INBOX.Processed') {
-    // move on server
         imap_mail_move($this->conn, $msg_index, $folder);
         imap_expunge($this->conn);
 
@@ -46,7 +69,12 @@ class Email_reader {
         $this->inbox();
     }
 
-    // get a specific message (1 = first email, 2 = second email, etc.)
+    /**
+     * get a specific message (1 = first email, 2 = second email, etc.)
+     * 
+     * @param type $msg_index
+     * @return type
+     */
     function get($msg_index = NULL) {
         if (count($this->inbox) <= 0) {
             return array();
@@ -57,11 +85,18 @@ class Email_reader {
         return $this->inbox;
     }
 
+    /**
+     * Delete email froms erver
+     * 
+     * @param type $msgNumber
+     */
     function delete($msgNumber) {
         imap_delete($this->conn, $msgNumber) or die('Cannot Delete: ' . print_r(imap_errors(), true));
     }
 
-    // read the inbox
+    /**
+     * Read the inbox
+     */
     function inbox() {
         $this->msg_cnt = imap_num_msg($this->conn);
 
